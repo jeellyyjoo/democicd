678650a3a7296d7d3a4720a39c7a59bf51bc5732
Andrei Tutski <tutskiandrei9@gmail.com>
2025-12-14 15:06:43 +0100

    added test containers

--- src/test/java/ragmvp/democicd/DemocicdApplicationTests.java
+++ src/test/java/ragmvp/democicd/DemocicdApplicationTests.java
@@ -2,12 +2,30 @@ package ragmvp.democicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class DemocicdApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("pgvector/pgvector:pg16")
+            .withDatabaseName("democicd_pg")
+            .withUsername("ttsk")
+            .withPassword("ttsk");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
